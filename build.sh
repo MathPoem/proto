7ccdb6b31f115e5c6c291a06056755f48f53f4d9
math <ilya.chervak.math@gmail.com>
2023-03-22 00:06:36 +0300

    init 3

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-protoc --go_out=. --go_opt=paths=source_relative\
-    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+protoc --go_out=samples --go_opt=module=github.com/MathPoem/proto/samples\
+    --go-grpc_out=samples --go-grpc_opt=module=github.com/MathPoem/proto/samples \
     samples/samples.proto
+
+#
+#    --go_opt=module=github.com/fgenetics/proto-contracts/clients
