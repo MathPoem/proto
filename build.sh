1fad4e1f36e8cd273a1740399b951f55bdf486fa
math <ilya.chervak.math@gmail.com>
2023-03-22 00:15:47 +0300

    init 3

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 protoc --go_out=samples --go_opt=module=github.com/MathPoem/proto/samples\
-    --go-grpc_out=samples --go-grpc_opt=module=github.com/MathPoem/proto/samples \
     samples/samples.proto
 
 #
